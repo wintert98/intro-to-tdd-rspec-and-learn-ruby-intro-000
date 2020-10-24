9b066e2802ab5029af4ae9e25b56eb32b117daeb
Thomas Winter <wintert98@gmail.com>
2020-10-24 19:08:14 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,5 +1,5 @@
-def current_age_for_birth_year(i)
+def current_age_for_birth_year(year)
   current_year = 2003
-  age_of_person = current_year - i
+  age_of_person = current_year - year
   puts age_of_person
 end
